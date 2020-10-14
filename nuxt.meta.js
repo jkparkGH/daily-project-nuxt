59e6acbd904d5a5e1ceef42f519ba0ce10469aa2
jake <pjklogs@naver.com>
2020-10-14 11:14:54 +0900

    - meta.js 수정

--- nuxt.meta.js
+++ nuxt.meta.js
@@ -4,23 +4,23 @@ module.exports = {
     { charset: 'utf-8' },
     { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
     { 'http-equiv': 'Content-Type', content: 'text/html; charset=utf-8' },
-    { name: 'author', content: 'TEST' },
-    { name: 'keywords', content: 'keyword' },
-    { hid: 'description', name: 'description', content: 'content' },
+    { name: 'author', content: 'JAKE' },
+    { name: 'keywords', content: 'Beauty House' },
+    { hid: 'description', name: 'description', content: 'Beauty House' },
     { name: 'format-detection', content: 'telephone=no' },
     { name: 'apple-mobile-web-app-capable', content: 'no' },
     { name: 'apple-mobile-web-app-status-bar-style', content: 'no' },
     { name: 'apple-mobile-web-app-title', content: 'black' },
-    { hid: 'og:url', property: 'og:url', content: 'test URL' },
-    { property: 'og:site_name', content: 'NAME' },
+    { hid: 'og:url', property: 'og:url', content: 'https://daily-beauty-house.netlify.app/' },
+    { property: 'og:site_name', content: 'BEAUTY HOUSE' },
     { property: 'og:type', content: 'website' },
-    { hid: 'og:title', property: 'og:title', content: 'content' },
+    { hid: 'og:title', property: 'og:title', content: 'BEAUTY HOUSE' },
     {
       hid: 'og:description',
       property: 'og:description',
       content: 'description'
     },
-    { hid: 'og:image', property: 'og:image', content: '' },
+    { hid: 'og:image', property: 'og:image', content: '/opengraph.png' },
     {
       name: 'viewport',
       content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, target-densitydpi=medium-dpi, user-scalable=0'
@@ -28,8 +28,8 @@ module.exports = {
     { name: 'theme-color', content: '#fe708a' }
   ],
   link: [
-    // { rel: 'icon', type: 'image/x-icon', href: '/favicon/icon.png' },
-    // { rel: 'apple-touch-icon-precomposed', href: '/favicon/icon.png' }
+    { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    { rel: 'apple-touch-icon-precomposed', href: '/favicon.ico' }
   ],
   script: [
     // { src: 'https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js' },
