5df13d902425431287afa347973e77c2197081df
jake <pjklogs@naver.com>
2020-10-14 11:06:24 +0900

    - meta.js 수정

--- nuxt.meta.js
+++ nuxt.meta.js
@@ -1,32 +1,31 @@
 module.exports = {
-  title: "BEAUTY HOUSE",
+  title: 'BEAUTY HOUSE',
   meta: [
-    { charset: "utf-8" },
-    { "http-equiv": "X-UA-Compatible", content: "IE=edge" },
-    { "http-equiv": "Content-Type", content: "text/html; charset=utf-8" },
-    { name: "author", content: "TEST" },
-    { name: "keywords", content: "keyword" },
-    { hid: "description", name: "description", content: "content" },
-    { name: "format-detection", content: "telephone=no" },
-    { name: "apple-mobile-web-app-capable", content: "no" },
-    { name: "apple-mobile-web-app-status-bar-style", content: "no" },
-    { name: "apple-mobile-web-app-title", content: "black" },
-    { hid: "og:url", property: "og:url", content: "test URL" },
-    { property: "og:site_name", content: "NAME" },
-    { property: "og:type", content: "website" },
-    { hid: "og:title", property: "og:title", content: "content" },
+    { charset: 'utf-8' },
+    { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
+    { 'http-equiv': 'Content-Type', content: 'text/html; charset=utf-8' },
+    { name: 'author', content: 'TEST' },
+    { name: 'keywords', content: 'keyword' },
+    { hid: 'description', name: 'description', content: 'content' },
+    { name: 'format-detection', content: 'telephone=no' },
+    { name: 'apple-mobile-web-app-capable', content: 'no' },
+    { name: 'apple-mobile-web-app-status-bar-style', content: 'no' },
+    { name: 'apple-mobile-web-app-title', content: 'black' },
+    { hid: 'og:url', property: 'og:url', content: 'test URL' },
+    { property: 'og:site_name', content: 'NAME' },
+    { property: 'og:type', content: 'website' },
+    { hid: 'og:title', property: 'og:title', content: 'content' },
     {
-      hid: "og:description",
-      property: "og:description",
-      content: "description"
+      hid: 'og:description',
+      property: 'og:description',
+      content: 'description'
     },
-    { hid: "og:image", property: "og:image", content: "" },
+    { hid: 'og:image', property: 'og:image', content: '' },
     {
-      name: "viewport",
-      content:
-        "width=device-width, initial-scale=1.0, maximum-scale=1.0, target-densitydpi=medium-dpi, user-scalable=0"
+      name: 'viewport',
+      content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, target-densitydpi=medium-dpi, user-scalable=0'
     },
-    { name: "theme-color", content: "#0443a1" }
+    { name: 'theme-color', content: '#fe708a' }
   ],
   link: [
     // { rel: 'icon', type: 'image/x-icon', href: '/favicon/icon.png' },
