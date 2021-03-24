51aa1b27bbdb4bb73139ddcc052aa4e346c3e32d
jake <pjklogs@naver.com>
2021-03-24 12:10:56 +0900

    refactor: email 정규표현식 수정

--- src/components/common/login-modal.vue
+++ src/components/common/login-modal.vue
@@ -59,7 +59,7 @@ export default {
   computed: {
     checkValidEmail() {
       const email = this.userEmail;
-      const regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z]).*.[a-zA-Z]{2,3}$/i;
+      const regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*[.][a-zA-Z]{2,3}$/i;
       return regExp.test(email);
     },
     checkValidPassword() {
